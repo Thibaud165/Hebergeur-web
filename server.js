4bb45b79d07e04803f5005a684dbf6bc32989c46
Thibaud <thibaudj165@gmail.com>
2025-09-10 20:24:38 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000; // ou 80 si tu veux pas mettre :3000
+
+const sitesDir = path.join(__dirname, "sites");
+
+// Lire tous les dossiers dans /sites
+fs.readdirSync(sitesDir).forEach(site => {
+  const sitePath = path.join(sitesDir, site);
+
+  if (fs.lstatSync(sitePath).isDirectory()) {
+    // Chaque site sera dispo sur http://localhost:3000/nomDuSite
+    app.use("/" + site, express.static(sitePath));
+    console.log(`📂 Site "${site}" disponible sur /${site}`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
+});
