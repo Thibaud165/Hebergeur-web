5e9b37ad1729d9f2ef6623b6f9a6dc80d1a521ef
Thibaud <thibaudj165@gmail.com>
2025-09-17 16:25:13 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,21 +2,38 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const domain = "http://thibaud.site/"
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
-const sitesDir = path.join(__dirname, "sites");
+// main site
 
-// Lire tous les dossiers dans /sites
+const mainSite = path.join(__dirname, "monSite");
+app.use("/", express.static(mainSite));
+console.log(`📁 Main site ready on /`);
+
+// other site
+
+const sitesDir = path.join(__dirname, "../Sites");
+
 fs.readdirSync(sitesDir).forEach(site => {
   const sitePath = path.join(sitesDir, site);
 
   if (fs.lstatSync(sitePath).isDirectory()) {
     app.use("/" + site, express.static(sitePath));
-    console.log(`📂 Site "${site}" disponible sur /${site}`);
+    console.log(`📂 Site "${site}" ready on ${domain}${site}`);
   }
 });
 
+// launch confirmation
+
 app.listen(PORT, () => {
-  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
+  let now = new Date();
+  let timeStr = `${now.getHours().toString().padStart(2,'0')}:` +
+                `${now.getMinutes().toString().padStart(2,'0')}:` +
+                `${now.getSeconds().toString().padStart(2,'0')}`;
+  console.log("")
+  console.log("🚀 Serveur launched at " + timeStr + " on :")
+  console.log("- http://localhost:" + PORT)
+  console.log("- http://thibaud.site")
 });
